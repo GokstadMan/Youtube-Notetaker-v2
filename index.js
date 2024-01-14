bc75db08ecf69e13eced83f20c3cdb4b0a033e02
Benjo <ben.ohman@gokstadakademiet.no>
2024-01-14 15:08:15 +0100

    created loadNotes

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-
+document.addEventListener("DOMContentLoaded",() =>{
 let videoPlayer = document.getElementById("video-player");
 let videoURL = document.getElementById("video-url-input");
 let watchBtn = document.getElementById("watch-button");
 let saveNoteBtn = document.getElementById("save-note-button");
-
 let noteInput = document.getElementById("video-note-taking");
 let notesList = document.getElementById("notes-list");
 let saveBtn = document.getElementById("save-note-button");
 let noNotesText = document.getElementById("no-notes-text");
+
+const loadNotes = () => {
+    const notes = JSON.parse(localStorage.getItem("notes")) || [];
+    notesList.innerHTML="";
+    notes.forEach((note,index) => {
+        const noteElement = createNoteElement(note,index);
+        notesList.appendChild(noteElement);
+    });
+    noNotesText.style.display = notes.length ? "none" : "block";
+} 
+});
+
