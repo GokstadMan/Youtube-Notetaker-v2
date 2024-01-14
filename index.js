0b2d3986872c1ed25ff31457368d509e19edf6a6
Benjo <ben.ohman@gokstadakademiet.no>
2024-01-14 14:41:15 +0100

    v2 higher code notetaker

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let videoPlayer = document.getElementById("video-player");
+let videoURL = document.getElementById("video-url-input");
+let watchBtn = document.getElementById("watch-button");
+let saveNoteBtn = document.getElementById("save-note-button");
+
+let noteInput = document.getElementById("video-note-taking");
+let notesList = document.getElementById("notes-list");
+let saveBtn = document.getElementById("save-note-button");
+let noNotesText = document.getElementById("no-notes-text");
